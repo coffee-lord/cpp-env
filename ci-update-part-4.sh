0e8b8b4c95106658349f692d0ccafa0fdd45c08d
Andrew <dronx.wow@gmail.com>
2017-08-22 21:12:35 +0300

    Add boost 1.65

--- ci-update-part-4.sh
+++ ci-update-part-4.sh
@@ -4,14 +4,14 @@ echo 'EXECUTING PART 4'
 
 cd $BOOST_ROOT/../
 
-curl -k -L --compressed 'https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2' > boost_1_64_0.tar.bz2
-tar -xf boost_1_64_0.tar.bz2
-rm boost_1_64_0.tar.bz2
+curl -k -L --compressed 'https://dl.bintray.com/boostorg/release/1.65.0/source/boost_1_65_0.tar.bz2' > boost_1_65_0.tar.bz2
+tar -xf boost_1_65_0.tar.bz2
+rm boost_1_65_0.tar.bz2
 
-cd boost_1_64_0
+cd boost_1_65_0
 ../build.sh
 
 cd ~
 
-rm -rf $BOOST_ROOT/../boost_1_64_0
+rm -rf $BOOST_ROOT/../boost_1_65_0
 rm -rf /tmp/boost
--- ci-update-part-6.sh
+++ ci-update-part-6.sh
@@ -4,7 +4,7 @@ echo 'EXECUTING PART 6'
 
 cd ~
 
-curl -k -L --compressed 'https://cmake.org/files/v3.9/cmake-3.9.0-rc5-Linux-x86_64.sh' > cmake.sh
+curl -k -L --compressed 'https://cmake.org/files/v3.9/cmake-3.9.1-Linux-x86_64.tar.gz' > cmake.sh
 chmod +x cmake.sh
 ./cmake.sh --prefix=/usr --exclude-subdir
 rm cmake.sh
